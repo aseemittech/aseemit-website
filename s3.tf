dbaacffde1387c63771996b875b39fc5f0cc400a
SabiNjoshi1 <sabinjoshi044@gmail.com>
2024-07-08 13:11:50 +0545

    dev added sns module

--- s3.tf
+++ s3.tf
@@ -3,17 +3,14 @@
 #################################################
 module "log_bucket" {
   source = "./modules/s3"
-
   bucket = local.log_bucket.bucket_name
   acl    = local.log_bucket.acl
   region = local.region
 
   # Allow deletion of non-empty bucket
-  force_destroy = local.log_bucket.force_destroy
-
-  control_object_ownership = local.log_bucket.control_object_ownership
-  object_ownership         = local.log_bucket.object_ownership
-
+  force_destroy                  = local.log_bucket.force_destroy
+  control_object_ownership       = local.log_bucket.control_object_ownership
+  object_ownership               = local.log_bucket.object_ownership
   attach_elb_log_delivery_policy = true
 }
 
